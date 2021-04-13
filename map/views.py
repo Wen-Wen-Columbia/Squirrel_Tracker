e63f44ebfbb6319d01ea5930bd8c9f838a7e1f72
Wen Wen <ww2573@columbia.edu>
2021-04-13 07:19:52 +0000

    Upload the function of Sighting Map

--- map/views.py
+++ map/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from sightings.models import Sight
 
-# Create your views here.
+def map_view(request):
+    sights = Sight.objects.all()[:100]
+    context = {
+            'sights':sights,
+            }
+    return render(request, 'map/map.html', context)
--- project/urls.py
+++ project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sightings.urls')),
+    path('map/', include('map.urls')),
 ]
